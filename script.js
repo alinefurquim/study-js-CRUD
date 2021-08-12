b0351670c73d8dc1668feeffb25049f168df4618
Aline Furquim <aline1080@gmail.com>
2021-08-12 16:20:02 -0300

    save and clear form

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const openModal = () => document.getElementById('modal').classList.add('active')
+
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active')
+    clearFields()
+}
+
 const registerClient = {
-    nome: "Aline Marial",
+    nome: "Lucas",
     email: "aline@study.com",
     celular: "(22)9999.9999",
     cidade: "Vancouver",
@@ -7,6 +14,12 @@ const registerClient = {
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem('database')) ?? []
 const setLocalStorage = (databaseClient) => localStorage.setItem('database', JSON.stringify(databaseClient))
+
+const deleteClient = (index) => {
+    const deleteDatabaseClient = readClient()
+    deleteDatabaseClient.splice(index, 1)
+    setLocalStorage(deleteDatabaseClient)
+}
 
 const updateClient = (index, client) => {
     const updatabaseClient = readClient()
@@ -21,3 +34,34 @@ const createClient = (client) => {
     databaseClient.push(client)
     setLocalStorage(databaseClient)    
 }
+
+const isValidFields = () => {
+   return document.getElementById('form').reportValidity()
+}
+
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
+
+const saveClient = () => {
+    if (isValidFields()) {
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value,
+        }
+        createClient(client)
+        clearFields()
+    }
+}
+
+document.getElementById('buttonRegisterClient')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
+
+document.getElementById('save')
+    .addEventListener('click', saveClient)
