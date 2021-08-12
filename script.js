197247ad291e9325d266a5bbcd578401bf4d83c2
Aline Furquim <aline1080@gmail.com>
2021-08-12 09:04:40 -0300

    database create

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const registerClient = {
-    nome: "Aline",
+    nome: "Aline Marial",
     email: "aline@study.com",
     celular: "(22)9999.9999",
     cidade: "Vancouver",
 }
 
+const getLocalStorage = () => JSON.parse(localStorage.getItem('database')) ?? []
+const setLocalStorage = (databaseClient) => localStorage.setItem('database', JSON.stringify(databaseClient))
+
+const updateClient = (index, client) => {
+    const updatabaseClient = readClient()
+    updatabaseClient[index] = client
+    setLocalStorage(updatabaseClient)
+}
+
+const readClient = () => getLocalStorage()
+
 const createClient = (client) => {
-    const databaseClient = []
+    const databaseClient = getLocalStorage()
     databaseClient.push(client)
-    localStorage.setItem('database', JSON.stringify(client))
+    setLocalStorage(databaseClient)    
 }
